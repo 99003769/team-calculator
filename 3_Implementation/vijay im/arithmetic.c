ff505722becfb328ca1e0a9400eee7134b7967aa
99003768g <78857841+99003768g@users.noreply.github.com>
2021-02-13 15:48:24 +0530

    Update arithmetic.c
    update

--- 3_Implementation/vijay im/arithmetic.c
+++ 3_Implementation/vijay im/arithmetic.c
@@ -14,7 +14,7 @@ int main()
    {
        case '+': 
        result=number_1+number_2;
-       printf("addition is %lf",result); 
+       printf("addition performed is %lf",result); 
                break; 
        case '-':
        result=number_1-number_2;
