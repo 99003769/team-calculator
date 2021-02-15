6cb84637a559ea30a025f219387ebf3cdef336b8
99003766 <78857458+99003766@users.noreply.github.com>
2021-02-15 11:36:15 +0530

    Update sqrt.c

--- 3_Implementation/jayasimha code/sqrt.c
+++ 3_Implementation/jayasimha code/sqrt.c
@@ -4,9 +4,7 @@
 int main() 
 {
    double n1, square_root;
-   {
-
-   printf("Enter a number: ");}
+   printf("Enter a number: ");
    scanf("%lf", &n1);
    square_root = sqrt(n1);
    printf("Square root of %.2f =  %.2f", n1, squar_root);
