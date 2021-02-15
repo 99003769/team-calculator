37afb0b3ff98622148d375a75244bb67b5916bba
99003768g <78857841+99003768g@users.noreply.github.com>
2021-02-15 14:15:08 +0530

    Create temperature_conversion_.h

---
+++ 3_Implementation/inc/temperature_conversion_.h
@@ -0,0 +1,21 @@
+/** 
+* @file calculator_operations.h
+* Calculator application with 4 mathematical operations
+*
+*/
+#ifndef __CALCULATOR_OPERATIONS_H__
+#define __CALCULATOR_OPERATIONS_H__
+#define __CALCULATOR_TEMPERATURE_CONVERSION_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+
+/**
+* converts fahrenheit to celsius and returns the result 
+* @param[in] input_temperature 
+* @return floating value of the conversion
+*/
+float temperature_conversion_f_to_c(float input_temperature);
+
+endif  /* #define __CALCULATOR_OPERATIONS_H__ */
