0db376cb216499064cd28d0df920f974211f5a49
99003766 <78857458+99003766@users.noreply.github.com>
2021-02-15 13:58:03 +0530

    Update Volume.h

--- 3_Implementation/inc/Volume.h
+++ 3_Implementation/inc/Volume.h
@@ -1,7 +1,6 @@
 #ifndef __VOLUME_CALCULATOR_H__
 #define __VOLUME_CALCULATOR_H__
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdio_ext.h>
+
 void volume_calc(int radius,int height,int side,char volume)
 #endif
