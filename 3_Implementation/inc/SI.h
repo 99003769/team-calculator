2139642b27e42e4a17ba354d3eee2faa574f8385
99003766 <78857458+99003766@users.noreply.github.com>
2021-03-10 22:42:18 +0530

    Update SI.h

--- 3_Implementation/inc/SI.h
+++ 3_Implementation/inc/SI.h
@@ -1,8 +1,8 @@
-#ifndef __SIMPLE_INTEREST_H__
-#define __SIMPLE_INTEREST_H__
+#ifndef __SI_H__
+#define __SI_H__
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdio_ext.h>
-void simple_interest(float time,float ROI,float simple_interest);
+
+float interest(int P,float R, int T)
 #endif
