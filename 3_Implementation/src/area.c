d45acefcf29020d7979e4db02b5eb44639e88f54
99003769 <78853972+99003769@users.noreply.github.com>
2021-03-10 22:32:11 +0530

    made changes

---
+++ 3_Implementation/src/area.c
@@ -0,0 +1,17 @@
+int rect_area(int length,int width)
+{
+     int area;
+     area=length*width;
+     return area;
+}
+int square_area(int length)
+{
+     int area;
+     area=length*length;
+     return area;
+}
+int triangle_area(int length,int width)
+{
+     int area;
+     area=0.5*length*width;
+     return area;
