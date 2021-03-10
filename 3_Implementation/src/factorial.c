1a80b34a9f753a586a50f19b94228f43a6ab9f94
99003769 <78853972+99003769@users.noreply.github.com>
2021-03-10 22:05:51 +0530

    some changes made in function

---
+++ 3_Implementation/src/factorial.c
@@ -0,0 +1,27 @@
+#include "factorial.h"
+
+
+error_t find_factorial(int num, long* result)
+{
+    if(1 == num)
+    {
+        return num;
+    }
+    else if (0 > num)
+    {
+        return NEGATIVE_NUMBER;
+    }
+    else if (0 == num)
+    {
+        return ZERO;
+    }
+    else
+    {
+        while ( 1 <= num )
+        {
+            *result *= num--;
+        }
+        return SUCCESS;
+    }
+    
+}
