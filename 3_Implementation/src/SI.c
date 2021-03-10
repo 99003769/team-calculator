7c3aa4850b04d3edde822e51febc9aa962360948
99003766 <78857458+99003766@users.noreply.github.com>
2021-03-10 22:40:51 +0530

    Update SI.c

--- 3_Implementation/src/SI.c
+++ 3_Implementation/src/SI.c
@@ -1,20 +1,8 @@
   
-#include<_SIMPLE_INTEREST.H>
 #include<stdio.h>
-
-
-
-
-  void simple_interest()
-  {
-    float principle_amount,time,ROI,simple_interest;
-    printf("Enter the principle_amount: ");
-    scanf("%f",&principle_amount);
-    printf("Enter the time: ");
-    scanf("%f",&time);
-    printf("Enter the rate of interest: ");
-    scanf("%f",&ROI);
-    simple_interest = (principle_amount * time * ROI) / 100;
-    printf("Simple Interest = %f", simple_interest);
-
-}
+float interest(int P,float R, int T)
+{
+     float SI;
+     SI=(P*T*R)/100;
+     return SI;
+}      
