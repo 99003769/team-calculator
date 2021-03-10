05e326d79c55a7348ec7a45130c8a0520e6b051b
99003766 <78857458+99003766@users.noreply.github.com>
2021-03-10 22:06:49 +0530

    Update sqrt.c

--- 3_Implementation/src/sqrt.c
+++ 3_Implementation/src/sqrt.c
@@ -1,17 +1,10 @@
-#include<SQRT_OPERATOR.H>
-#include<stdio.h>
-#include<math.h>
-
-
-
-  void sqrt_operator()
-  {
-      double n1,square_root;
-
-   printf("Enter a number: ");
-   scanf("%lf", &n1);
-   square_root = sqrt(n1);
-   printf("Square root of %.2f =  %.2f", n1, square_root);
-
-  }
+double square_Root (double n)
+{
+  
+double k, precision = 0.001;
+for(k = 1; k*k <=n; ++k);
+for (--k; k*k < n; k += precision); 
+return k;
+  
+}
 
