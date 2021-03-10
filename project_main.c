1568279ffbc2945095294adde657ed1e371b2747
99003769 <78853972+99003769@users.noreply.github.com>
2021-03-10 23:00:44 +0530

    Create project_main.c

---
+++ project_main.c
@@ -0,0 +1,13 @@
+#include "factorial.h"
+#include "area.h"
+
+int main()
+{
+    int x,y,z,fact;
+    factfind_factorial(5);
+    x=rect_area(5,6); // calling function rect_area
+    x=square_area(5);
+    z=triangle_area(5,6);
+    return 0;
+}
+ 
