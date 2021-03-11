b752edf742619a90a324e223ceb6dfcf02aa961e
99003770 <78854076+99003770@users.noreply.github.com>
2021-03-11 13:29:04 +0530

    Update matrix.c

--- 3_Implementation/src/matrix.c
+++ 3_Implementation/src/matrix.c
@@ -10,18 +10,3 @@ float addition(float num_1, float num_2)
    return sum;
 }
 
-int main()
-{
-   
-   float number1, number2, result;
-
-   printf("Enter two number: ");
-   scanf("%f %f",&number1, &number2);
-
-   result = addition(number1, number2);
-
-   printf("%.2f + %.2f = %.2f\n",
-               number1, number2, result);
-
-   return 0;
-}
