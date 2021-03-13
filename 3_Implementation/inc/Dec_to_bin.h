7f3ad29c36cd3f8e1002b6c5aa25a5c5a4a85a1c
99003770 <78854076+99003770@users.noreply.github.com>
2021-03-13 10:28:00 +0530

    Add files via upload

---
+++ 3_Implementation/inc/Dec_to_bin.h
@@ -0,0 +1,7 @@
+#ifndef __DEC_TO_BIN_H__
+#define __DEC_TO_BIN_H__
+#include<stdio.h>
+
+long dectobin(int num);
+
+#endif
---
+++ 3_Implementation/inc/profit_loss.h
@@ -0,0 +1,9 @@
+#ifndef _PROFIT_LOSS_H__
+#define _PROFIT_LOSS_H__
+
+#include<stdio.h>
+
+int profit(int costprice, int sellingprice);
+int loss(int costprice, int sellingprice);
+
+#endif
