a90d64781a2dc67ed6a0d7158bf188bfad0c48f9
99003770 <78854076+99003770@users.noreply.github.com>
2021-03-13 10:28:33 +0530

    Delete profit_loss.c

--- 3_Implementation/src/profit_loss.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int profit(int costprice, int sellingprice)
-{
-    return (sellingprice - costprice);
-}
-int loss(int costprice, int sellingprice)
-{
-    return (costprice - sellingprice);
-}
